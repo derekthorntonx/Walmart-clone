c0930177e69327cd9a294e07773bc574539fce47
Derek <derek-thornton@hotmail.ca>
2023-01-06 23:16:32 -0500

    added filter states

--- src/views/Main.js
+++ src/views/Main.js
@@ -11,8 +11,42 @@ function Main() {
     const [brandShow, setBrandShow] = useState(false); 
     const [priceShow, setPriceShow] = useState(false);
     const [memoryShow, setMemoryShow] = useState(false); 
-    const [harddriveShow, setHarddriveShow] = useState(false); 
-    
+    const [harddriveShow, setHarddriveShow] = useState(false);
+    const [ratingFilter, setRatingFilter] = useState(false);
+    const [brandFilter, setBrandFilter] = useState([]);
+    const [priceFilter, setPriceFilter] = useState([]);
+    const [memoryFilter, setMemoryFilter] = useState([]);
+    const [harddriveFilter, setHarddriveFilter] = useState(false);
+    const [minPrice, setMinPrice] = useState(false);
+    const [maxPrice, setMaxPrice] = useState(false);
+
+    function handleBrandSelection(e) {
+        if (e.target.checked){
+            setBrandFilter([...brandFilter, e.target.value])
+        }
+        if (!e.target.checked){
+            setBrandFilter(brandFilter.filter(a => a !== `${e.target.value}`))
+        }
+    }
+
+    function handlePriceRange(e) {
+        e.preventDefault();
+        if (minPrice > maxPrice){
+            alert('Minimum price cannot be higher than maximum.')
+            setMaxPrice(false);
+            return;
+        }
+        setPriceFilter([minPrice, maxPrice]);
+    }
+
+    function handleMemorySelection(e) {
+        if (e.target.checked){
+            setMemoryFilter([...memoryFilter, e.target.value])
+        }
+        if (!e.target.checked){
+            setMemoryFilter(memoryFilter.filter(a => a !== `${e.target.value}`))
+        }
+    }
 
   return (
     <div className="App">
@@ -35,29 +69,28 @@ function Main() {
             <h1 style={{color: '#333333', maxHeight: '125px'}}>Gaming laptop</h1>
 
             <div className='filter-menu'>
-                <div>list of filters</div>
                 <ul>
                     <li>
                         <div style={{display: 'flex', justifyContent: 'space-between', paddingRight: '10%'}} onClick={() => setRatingShow(!ratingShow)}><h4>Customer Rating</h4><span style={{color: '#007dc6'}}><FontAwesomeIcon icon={ratingShow ? faMinus : faPlus}/></span></div>
-                        {ratingShow ? <form className='filter-form'>
+                        {ratingShow ? <form className='filter-form' onChange={(e)=> {setRatingFilter(e.target.value)}}>
                             <div>
-                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='fiveStar' value='fiveStar'/>
+                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='fiveStar' value='5'/>
                                 <label htmlFor='fiveStar'><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/></label>
                             </div>
                             <div>
-                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='fourStar' value='fourStar'/>
+                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='fourStar' value='4'/>
                                 <label htmlFor='fourStar'><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/>+</label>
                             </div>
                             <div>
-                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='threeStar' value='threeStar'/>
+                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='threeStar' value='3'/>
                                 <label htmlFor='threeStar'><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/>+</label>
                             </div>
                             <div>
-                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='twoStar' value='twoStar'/>
+                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='twoStar' value='2'/>
                                 <label htmlFor='twoStar'><FontAwesomeIcon className='fa-sm' icon={faStar}/><FontAwesomeIcon className='fa-sm' icon={faStar}/>+</label>
                             </div>
                             <div>
-                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='oneStar' value='oneStar'/>
+                                <input style={{marginRight: '2.5%'}} type='radio' name='rating' id='oneStar' value='1'/>
                                 <label htmlFor='oneStar'><FontAwesomeIcon className='fa-sm' icon={faStar}/>+</label>
                             </div>
                         </form> : null}
@@ -65,7 +98,7 @@ function Main() {
 
                     <li>
                     <div onClick={() => setBrandShow(!brandShow)} style={{display: 'flex', justifyContent: 'space-between', paddingRight: '10%'}}><h4>Brand</h4><span style={{color: '#007dc6'}}><FontAwesomeIcon icon={brandShow ? faMinus : faPlus}/></span></div>
-                        {brandShow ? <form className='filter-form'>
+                        {brandShow ? <form className='filter-form' onChange={handleBrandSelection}>
                             <div>
                                 <input type='checkbox' id='MSI' name='msi' value='MSI'/>
                                 <label style={{marginLeft: '2.5%'}} htmlFor='MSI'>MSI</label>
@@ -88,15 +121,15 @@ function Main() {
                     <li>
                     <div onClick={() => setPriceShow(!priceShow)} style={{display: 'flex', justifyContent: 'space-between', paddingRight: '10%'}}><h4>Price</h4><span style={{color: '#007dc6'}}><FontAwesomeIcon icon={priceShow ? faMinus : faPlus}/></span></div>
                         {priceShow ? <form className='filter-form'>
-                            <input type='number' placeholder='Min' value='minimumPrice'/>
-                            <input type='number' placeholder='Max' value='maximumPrice'/>
-                            <button style={{width: '50%', marginTop: '5%', marginLeft: '25%'}} className='add-to-cart-button'>Apply</button>
+                            <input type='number' placeholder='Min' value={minPrice} onChange={(e) => setMinPrice(e.target.value)}/>
+                            <input type='number' placeholder='Max' value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)}/>
+                            <button onClick={handlePriceRange} style={{width: '50%', marginTop: '5%', marginLeft: '25%'}} className='add-to-cart-button'>Apply</button>
                         </form> : null}
                     </li>
 
                     <li>
                     <div onClick={() => setMemoryShow(!memoryShow)} style={{display: 'flex', justifyContent: 'space-between', paddingRight: '10%'}}><h4>RAM</h4><span style={{color: '#007dc6'}}><FontAwesomeIcon icon={memoryShow ? faMinus : faPlus}/></span></div>
-                        {memoryShow ? <form className='filter-form'>
+                        {memoryShow ? <form className='filter-form' onChange={handleMemorySelection}>
                             <div>
                                 <input type='checkbox' id='fourGB' name='fourGB' value='4'/>
                                 <label style={{marginLeft: '2.5%'}} htmlFor='fourGB'>4 GB</label>
@@ -118,7 +151,7 @@ function Main() {
 
                     <li>
                     <div onClick={() => setHarddriveShow(!harddriveShow)} style={{display: 'flex', justifyContent: 'space-between', paddingRight: '10%'}}><h4>Hard Drive Type</h4><span style={{color: '#007dc6'}}><FontAwesomeIcon icon={harddriveShow ? faMinus : faPlus}/></span></div>
-                        {harddriveShow ? <form className='filter-form'>
+                        {harddriveShow ? <form className='filter-form' onChange={(e)=> {setHarddriveFilter(e.target.value)}}>
                             <div>
                                 <input type='radio' name='hardDriveType' id='hdd' value='hdd'></input>
                                 <label style={{marginLeft: '2.5%'}} htmlFor='hhd'>HDD</label>
